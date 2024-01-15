95da39071933d6ed6893b4e3fd2554f97c78361d
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-15 19:21:59 +0100

    petle

--- 01_Wprowadzenie/Zadanie_02/main.js
+++ 01_Wprowadzenie/Zadanie_02/main.js
@@ -1,6 +1,7 @@
 console.log("lubię frytki :)");
 
 const a = 1; // Zdefiniowanie zmiennej
-const b = 2; // Zdefiniowanie zmiennej
+const
+    b = 2; // Zdefiniowanie zmiennej
 const c = "Ala"; // Zdefiniowanie zmiennej
 console.log(a, b, c); // Wypisanie wartości
