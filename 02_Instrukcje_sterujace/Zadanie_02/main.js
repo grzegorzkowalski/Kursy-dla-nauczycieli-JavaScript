83d1cd01c9f89e5de79dfc72e795f59f4edef02c
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-15 20:07:31 +0100

    Instrukcje sterujace

---
+++ 02_Instrukcje_sterujace/Zadanie_02/main.js
@@ -0,0 +1,10 @@
+const n = 30;
+
+for (let i = 0; i <= n; i++) {
+    if (i % 2 === 0) {
+        console.log( i + " - even");
+    }
+    else {
+        console.log( i + " - odd");
+    }
+}
--- 02_Instrukcje_sterujace/Zadanie_03/main.js
+++ 02_Instrukcje_sterujace/Zadanie_03/main.js
@@ -1,1 +1,9 @@
 const cake = "🎂";
+
+const age = new Date().getFullYear() - 1986;
+
+console.log(age);
+
+for (let i = 0; i < age; i++) {
+    console.log(i+1 + " - " + cake)
+}
