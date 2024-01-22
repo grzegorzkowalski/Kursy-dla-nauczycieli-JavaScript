5e2bfdf7d198730f586d84d830f039f4e893d434
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-22 19:13:53 +0100

    Chapter 3 Task 4

--- 03_Tablice/Zadanie_04/main.js
+++ 03_Tablice/Zadanie_04/main.js
@@ -3,3 +3,8 @@ const numbers = [1, 1000, 101, 11];
 function compareNumbers(a, b) {
    return a - b;
 }
+
+console.log(numbers.sort(compareNumbers));
+//console.log(numbers.toSorted(compareNumbers), "toSorted"); <= funkcja niemutacyjna (niezmieniająca oryginalnej tablicy)
+console.log([8,9,2,3,11, 101].sort(compareNumbers));
+console.log(numbers, "numbers");
