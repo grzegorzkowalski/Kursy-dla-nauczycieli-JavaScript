1f592789430732b29395a68936b91003c5c5dc2e
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-22 18:11:41 +0100

    Chapter 3 Task 1

---
+++ 03_Tablice/Zadanie_01/main.js
@@ -0,0 +1,20 @@
+// Na koniec wypisz w terminalu w kolejnych liniach:
+//
+// pierwszy element
+//
+// drugi element
+//
+// czwarty element
+//
+// ostatni element
+//
+// długość tablicy
+
+const users = ["Anna", "Maria", "Tomasz", "Piotr", "Paweł"];
+
+console.log(users[0], "- Pierwszy element");
+console.log(users[1], "- Drugi element");
+console.log(users[3], "- Czwarty element");
+console.log(users[users.length - 1], "- Ostatni element");
+console.log(users.length, "- Długość tablicy");
+
