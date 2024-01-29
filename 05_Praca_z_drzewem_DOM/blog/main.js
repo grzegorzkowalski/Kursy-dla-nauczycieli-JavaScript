b3159dd62d3812135d3c64f2047b8cbfcb4af3ee
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-01-29 20:25:51 +0100

    Drzewo Dom zadania 1,2, czesc 3

--- 05_Praca_z_drzewem_DOM/blog/main.js
+++ 05_Praca_z_drzewem_DOM/blog/main.js
@@ -4,3 +4,27 @@ console.log(header);
 
 const picture = document.querySelector("img");
 console.log(picture);
+
+const blogForm = document.querySelector("form");
+console.log(blogForm);
+
+const mail = document.querySelector(".e-mail a");
+console.log(mail);
+
+//Zadanie2
+
+const galleryItems = document.querySelectorAll(".gallery-main-photo");
+console.log(galleryItems);
+const galleryArray = Array.from(galleryItems);
+console.log(galleryArray, "galleryArray");
+//galleryItems.forEach(el => console.log(el));
+
+//Zadanie 3
+
+console.log(header.textContent);
+header.textContent = "Hello world!";
+console.log(picture.tagName, "tagName");
+const wwsiLogo = "https://wwsi.edu.pl/wp-content/themes/hello-theme-child-master/assets/imgs/logo-300x81.png";
+picture.setAttribute("src", wwsiLogo);
+
+//Dokończyć wspólnie od podpunktu 4
