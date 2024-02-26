8eb1de95888c89a4e9cd126924763f8264df17a1
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-02-26 20:33:56 +0100

    React start

--- 08_React_rozpoczecie_pracy/react-kurs/src/App.jsx
+++ 08_React_rozpoczecie_pracy/react-kurs/src/App.jsx
@@ -1,14 +1,77 @@
+import Artist from "./data/artist.js";
+import Fruits from "./data/fruits.js";
 import './App.css'
 
 function App() {
-const numA = parseInt(prompt("Podaj pierwszą liczbę?"));
-console.log(numA);
-const numB = parseInt(prompt("Podaj drugą liczbę?"));
-console.log(numB);
+// const numA = parseInt(prompt("Podaj pierwszą liczbę?"));
+// const numB = parseInt(prompt("Podaj drugą liczbę?"));
+// const operator = prompt("Wybierz operacje +,-,*,/?");
+// let result = "";
+// switch (operator) {
+//     case "+":
+//         result = <h1>{numA+numB}</h1>;
+//         break;
+//     case "-":
+//         result = <h2>{numA-numB}</h2>;
+//         break;
+//     case "*":
+//         result = <h3>{numA*numB}</h3>;
+//         break;
+//     case "/":
+//         result = <h4>{numA/numB}</h4>;
+//         break;
+//     default:
+//         result = <h5>Błędny wybór, spróbuj jeszcze raz</h5>;
+// }
+// const year = parseInt(prompt("Podaj swój rok urodzenia"));
+
+ //Zadanie nr 2 - Atrybuty
+    const randomNumber = () => {
+        return Math.round(Math.random() * 9 + 1);
+    }
+    const randOne = randomNumber();
+    const randomTwo = randomNumber();
+
+    const sum = parseInt(prompt(`Jaka jest suma liczby ${randOne} + ${randomTwo}?`));
+
+    const sumResult = sum === randOne + randomTwo;
+
+
+    //Zadanie nr 4 - Atrybuty
+
+    const color = prompt("Wybierz kolor red/green/blue?");
+    let colorDiv = "";
+    const colors = ["red", "green", "blue"];
+    const isOk = colors.includes(color);
+    if (isOk) {
+        colorDiv = <div style={{
+            width: "100px",
+            height: "100px",
+            borderStyle: "solid",
+            borderColor: color
+        }}></div>;
+    }
+    else {
+        colorDiv = <div>Błędny wybór</div>;
+    }
 
     return (
     <>
-        <h1>{numA + numB}</h1>
+        <div style={{backgroundColor: sumResult ? "green" : "red"}}>
+            <p>{sumResult ? "Poprawna odpowiedź" : "Niepoprawna odpowiedź"}</p>
+        </div>
+        {colorDiv}
+        {/*<h1>{numA + numB}</h1>*/}
+        {/*<h2>{new Date().getFullYear() - year}</h2>*/}
+        <ul>
+            <li>{Artist.nickname}</li>
+            <li>{Artist.firstName}</li>
+            <li>{Artist.lastName}</li>
+            <li>{Artist.age}</li>
+        </ul>
+        {/*<div>{result}</div>*/}
+        <p>Owoce: {Fruits.length}</p>
+        <p>{Fruits.join("-")}</p>
     </>
   )
 }
